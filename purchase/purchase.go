502db93bf61c1c73d7b774d862f91b6fafefb1e6
Teddy Wing <git@teddywing.com>
2015-06-07 19:38:02 -0400

    purchase.go: Split `SendMoney` declaration onto 2 lines
    Makes it easier to read. Previously I had trouble trying to do this
    because I put the argument list on the second line by itself but you
    can't do that. You need to separate at a comma inside a set of
    parentheses in order to compile.

--- purchase/purchase.go
+++ purchase/purchase.go
@@ -4,7 +4,8 @@ import (
 	"github.com/teddywing/new-house-on-the-block/vendor/_nuts/github.com/fabioberger/coinbase-go"
 )
 
-func SendMoney(from_key string, from_secret string, to string, amount string) (transaction_id string, err error) {
+func SendMoney(from_key string, from_secret string,
+		to string, amount string) (transaction_id string, err error) {
 	c := coinbase.ApiKeyClientSandbox(from_key, from_secret)
 
 	params := &coinbase.TransactionParams{
